2c5dd5b3265657ba8b5c9774280d9c7b278216e4
prathiksha <119397581+prathi1282@users.noreply.github.com>
2025-07-01 20:43:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,4 @@ def home():
     return "Hello, welcome to my Dockerized Flask app on AWS!"
 
 if _name_ == "_main_":
-    app.run(host='0.0.0.0', port=5000)
-
-
----
+    app.run(host="0.0.0.0", port=5000)
