17e65155ef6d9db701ab58822bd1ad04cfedb9e3
prathi <prathikshamarati2002@gmail.com>
2025-07-01 18:53:11 +0530

    initial commit - dockerized flask app

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+app = Flask(_name_)
+
+@app.route('/')
+def home():
+    return "Hello, welcome to my Dockerized Flask app on AWS!"
+
+if _name_ == "_main_":
+    app.run(host='0.0.0.0', port=5000)
+
+
+---
